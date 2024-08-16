7ad6a59753cbd7959ae022d825f5852a25dcf888
jisoo <zlsoo9ee@gmail.com>
2024-08-16 22:09:52 +0900

    [구현] 럭키 스트레이트 (#8)

--- 지수/구현/impl_럭키스트레이트.java
+++ 지수/구현/impl_럭키스트레이트.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class impl_럭키스트레이트 {
     private static String str;
     private static int summary = 0;
-
+  
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         str = br.readLine();
