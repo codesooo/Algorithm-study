29e7afdb823d721ef146652876e695f98eb32914
jisoo <zlsoo9ee@gmail.com>
2024-09-03 17:44:17 +0900

    [정렬] 성적이 낮은 순서로 학생 출력하기 (#22)

--- 지수/정렬/sort_성적이낮은순서로학생출력하기.java
+++ 지수/정렬/sort_성적이낮은순서로학생출력하기.java
@@ -6,7 +6,7 @@ class Student implements Comparable<Student> {
     private String name;
     private int score;
 
-    public Student(String name, int score) {
+    public Student(String name, int score) { 
         this.name = name;
         this.score = score;
     }
